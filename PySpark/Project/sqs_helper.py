c8ad6f69291242a1095e37ee57d4c0a679e0d389
venky <kvincloud@gmail.com>
2020-06-24 06:35:52 -0400

    sqs helper file description changed

--- PySpark/Project/sqs_helper.py
+++ PySpark/Project/sqs_helper.py
@@ -1,3 +1,7 @@
 """
-This is to help with create Queues, sending messages to queues, read messages, delete queues, list queues
+This is to help with create Queues,
+sending messages to queues,
+read messages,
+delete queues,
+list queues
 """
